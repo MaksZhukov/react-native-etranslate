514cd6d9aee0d6b732c5f1572b9a3a9d1336485a
MaksZhukov <maks_zhukov_97@mail.ru>
2020-10-30 23:19:08 +0300

    Added voice recognition

--- api/translator.js
+++ api/translator.js
@@ -1,6 +1,13 @@
 import api from './index';
 const translate = (query) => api(`translate`, { params: query });
+const speechToText = (data) =>
+    api.post(`speech`, data, {
+        headers: {
+            // 'Content-Type': 'multipart/form-data',
+        },
+    });
 
 export default {
     translate,
+    speechToText,
 };
--- screens/Translator.js
+++ screens/Translator.js
@@ -1,21 +1,35 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { inject, observer } from 'mobx-react';
-import {
-    Container,
-    Form,
-    Input,
-    Button,
-    Item,
-    Icon,
-    Textarea,
-    Picker,
-    View,
-} from 'native-base';
+import { Container, Button, Icon, Textarea, Picker, View } from 'native-base';
+import { Audio } from 'expo-av';
+import * as Permissions from 'expo-permissions';
 import * as Speech from 'expo-speech';
+import * as FileSystem from 'expo-file-system';
 import apiTranslator from '../api/translator';
 import i18n from '../locale';
 import { debounce } from '../helpers';
 import { LANGUAGES } from '../constants';
+
+const recordingOptions = {
+    android: {
+        extension: '.mp4',
+        outputFormat: Audio.RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_MPEG_4,
+        audioEncoder: Audio.RECORDING_OPTION_ANDROID_AUDIO_ENCODER_AAC,
+        sampleRate: 44100,
+        numberOfChannels: 1,
+        bitRate: 128000,
+    },
+    ios: {
+        extension: '.wav',
+        audioQuality: Audio.RECORDING_OPTION_IOS_AUDIO_QUALITY_HIGH,
+        sampleRate: 44100,
+        numberOfChannels: 1,
+        bitRate: 128000,
+        linearPCMBitDepth: 16,
+        linearPCMIsBigEndian: false,
+        linearPCMIsFloat: false,
+    },
+};
 
 const Translator = ({ user: { locale }, navigation }) => {
     let [text, setText] = useState('');
@@ -26,6 +40,8 @@ const Translator = ({ user: { locale }, navigation }) => {
     let [loading, setLoading] = useState(false);
     let [translation, setTranslation] = useState('');
     let [availableVoices, setAvailableVoices] = useState([]);
+    let [isRecording, setIsRecording] = useState(false);
+    const recording = useRef(null);
 
     useEffect(() => {
         const fetchAvailableVoices = async () => {
@@ -68,7 +84,80 @@ const Translator = ({ user: { locale }, navigation }) => {
         let objectVoice = availableVoices.find((item) =>
             item.language.toLowerCase().includes(language)
         );
-        Speech.speak(text, { voice: objectVoice.identifier });
+        Speech.speak(text);
+    };
+
+    const getRecordingTranscription = async () => {
+        try {
+            const { uri } = await FileSystem.getInfoAsync(
+                recording.current.getURI()
+            );
+            const formData = new FormData();
+            formData.append('file', {
+                uri,
+                type: 'audio/m4a',
+                name: `${Date.now()}.m4a`,
+            });
+            formData.append('textLang', selectedTextLang);
+            formData.append('translateLang', selectedTranslateLang);
+            const { data } = await apiTranslator.speechToText(formData);
+            FileSystem.deleteAsync(uri);
+            console.log(data);
+        } catch (error) {
+            console.log('getRecording transcription', error);
+        }
+    };
+
+    const stopRecording = async () => {
+        setIsRecording(false);
+        try {
+            await recording.current.stopAndUnloadAsync();
+            getRecordingTranscription();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const startRecording = async () => {
+        setIsRecording(true);
+        await Audio.setAudioModeAsync({
+            allowsRecordingIOS: true,
+            interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+            playsInSilentModeIOS: true,
+            interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+            playThroughEarpieceAndroid: true,
+        });
+        const record = new Audio.Recording();
+        try {
+            await record.prepareToRecordAsync(
+                Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+            );
+            await record.startAsync();
+        } catch (error) {
+            stopRecording();
+        }
+        recording.current = record;
+    };
+
+    const handlePressRecordVoice = async () => {
+        const { status } = await Permissions.askAsync(
+            Permissions.AUDIO_RECORDING
+        );
+        if (status !== 'granted') return;
+        if (isRecording) {
+            stopRecording();
+        } else {
+            startRecording();
+        }
+    };
+
+    const handleTextValuePickerChange = (val) => {
+        setSelectedTextLang(val);
+        setSelectedTranslateLang(selectedTextLang);
+    };
+    const handleTranslateValuePickerChange = (val) => {
+        setSelectedTranslateLang(val);
+        setSelectedTextLang(selectedTranslateLang);
     };
 
     return (
@@ -79,7 +168,7 @@ const Translator = ({ user: { locale }, navigation }) => {
                     style={{
                         width: '50%',
                     }}
-                    onValueChange={setSelectedTextLang}
+                    onValueChange={handleTextValuePickerChange}
                     selectedValue={selectedTextLang}
                     mode='dropdown'>
                     {LANGUAGES.map((item) => (
@@ -98,6 +187,20 @@ const Translator = ({ user: { locale }, navigation }) => {
                     bordered>
                     <Icon name='volume-high' />
                 </Button>
+                <Button
+                    onPress={handlePressRecordVoice}
+                    style={{
+                        margin: 5,
+                        width: 52,
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                    rounded
+                    transparent
+                    bordered>
+                    <Icon name={isRecording ? 'mic' : 'mic-off'} />
+                </Button>
             </View>
             <Textarea
                 style={{ flex: 4 }}
@@ -109,7 +212,7 @@ const Translator = ({ user: { locale }, navigation }) => {
                 <Picker
                     note
                     style={{ width: '50%' }}
-                    onValueChange={setSelectedTranslateLang}
+                    onValueChange={handleTranslateValuePickerChange}
                     selectedValue={selectedTranslateLang}
                     mode='dropdown'>
                     {LANGUAGES.map((item) => (
