9fd56ab89ee1cd4b6768491f058daec71f81fd96
MaksZhukov <maks_zhukov_97@mail.ru>
2020-11-03 23:52:42 +0300

    Added dictionary accordion with removing from dictionary functionality

--- screens/Dictionary.js
+++ screens/Dictionary.js
@@ -1,21 +1,51 @@
 import React, { useLayoutEffect } from 'react';
 import { inject, observer } from 'mobx-react';
-import { Container, Text, Button } from 'native-base';
+import { Container, Text, Accordion, View, Icon } from 'native-base';
 
-const Dictionary = ({ user, userDictionary: { items }, navigation }) => {
+const Dictionary = ({
+    user,
+    userDictionary: { items, removeFromUserDictionary },
+    navigation,
+}) => {
     useLayoutEffect(() => {
         navigation.setOptions({
             headerLeft: null,
         });
     }, [navigation]);
+    const dataArray = items.map((item) => ({
+        id: item.id,
+        title: `${item.textLang} | ${item.text} | ${item.translateLang}`,
+        content: item.translate,
+    }));
+
+    const renderHeader = (item, expanded) => {
+        return (
+            <View
+                key={item.id}
+                style={{
+                    flexDirection: 'row',
+                    paddingHorizontal: 15,
+                    paddingVertical: 10,
+                    alignItems: 'center',
+                    backgroundColor: 'lightgray',
+                }}>
+                <Text style={{ flex: 1 }}> {item.title}</Text>
+                <Icon
+                    onPress={() => removeFromUserDictionary([item.id])}
+                    style={{ fontSize: 20, color: '#e07c40' }}
+                    name='trash'
+                />
+            </View>
+        );
+    };
     return (
         <Container>
-            {items.map((item) => (
-                <>
-                    <Text>{item.text}</Text>
-                    <Text>{item.translate}</Text>
-                </>
-            ))}
+            <Accordion
+                icon='add'
+                expandedIcon='remove'
+                dataArray={dataArray}
+                renderHeader={renderHeader}
+            />
         </Container>
     );
 };
